98c7b4ec6adf9f800d36509852b26e5a1208a87b
stella <yinglan165@gmail.com>
2022-04-28 03:06:01 -0500

    fix fuel price calculation

--- static/suggested.js
+++ static/suggested.js
@@ -4,8 +4,9 @@ function get_suggest()
     
      //get data from front end using "ID" attribute
     var amount = parseInt(document.getElementById("num-gallons").value);
-    var instate = document.getElementById("inoutstate").innerHTML;
-    var reqb4 = parseInt(document.getElementById("reqb4").innerHTML);
+    //var address2 = document.getElementById("inoutstate").innerHTML;
+    var instate = document.getElementById("inoutstate").value;
+    var reqb4 = document.getElementById("reqb4").value;
     var price_back = document.getElementById("price");
     var total_back =document.getElementById("total");
 
@@ -19,13 +20,13 @@ function get_suggest()
         {GReqfactor=0.03;}
 
 
-    if (instate==1 && reqb4 ==1){
+    if (instate== "True" && reqb4 =="False"){
         finalNum =(0.02-0.01+GReqfactor+0.1) * 1.5;}
-    else if (instate == 0 && reqb4 ==1){
+    else if (instate == "False" && reqb4 =="True"){
         finalNum = (0.04-0.01+GReqfactor+0.1) * 1.5;}
-    else if( instate ==1 && reqb4==0){
+    else if( instate =="True" && reqb4=="False"){
         finalNum = (0.02+GReqfactor+0.1) * 1.5;}
-    else if (instate==0 && reqb4 ==0){
+    else if (instate=="False" && reqb4 =="False"){
         finalNum =(0.04+GReqfactor+0.1) * 1.5;}
     //else
       //  finalNum = 100000;
@@ -34,24 +35,13 @@ function get_suggest()
 
    getsug=1.5+finalNum;
    payme=getsug*amount;
-   payme.toFixed(2);//fix decimal
+   sum=parseFloat(payme).toFixed(2);//fix decimal to 2 places
 
-   instate+="love";
-   price_back.innerHTML=instate;
-   total_back.innerHTML=payme;
+   //instate+="love";
+   //address2+="hate";
+   price_back.innerHTML=getsug;
+   total_back.innerHTML=sum;
 
-/*
-if(instate==0 && reqb4==0)
-    finalNum=200;
-
-else
-    finalNum= 1000;
-
-
-price_back.innerHTML=finalNum;
-total_back.innerHTML=500;
-*/
-    
 
    var server_data =[
        {"amount":amount},
