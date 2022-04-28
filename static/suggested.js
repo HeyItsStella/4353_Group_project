fbfdd6a18237170bd0594cc9b16a5ec58045e89a
stella <yinglan165@gmail.com>
2022-04-28 15:53:25 -0500

    check for empty value

--- static/suggested.js
+++ static/suggested.js
@@ -3,8 +3,9 @@ function get_suggest()
 {  
     
      //get data from front end using "ID" attribute
-    var amount = parseInt(document.getElementById("num-gallons").value);
+    var amount = document.getElementById("num-gallons").value;
     //var address2 = document.getElementById("inoutstate").innerHTML;
+    var date2=document.getElementById("date").value;
     var instate = document.getElementById("inoutstate").value;
     var reqb4 = document.getElementById("reqb4").value;
     var price_back = document.getElementById("price");
@@ -14,50 +15,57 @@ function get_suggest()
     getsug =0;
     payme =0;
 
-    if (amount>1000)
-        {GReqfactor =0.02;}
-    else
-        {GReqfactor=0.03;}
+    if(amount=='' || date2=='' ){
+        window.alert("Please enter amount and delivery date");
+        return}
+
+    else {
+        
+        if (amount>1000)
+            {GReqfactor =0.02;}
+        else
+            {GReqfactor=0.03;}
 
 
-    if (instate== "True" && reqb4 =="False"){
-        finalNum =(0.02-0.01+GReqfactor+0.1) * 1.5;}
-    else if (instate == "False" && reqb4 =="True"){
-        finalNum = (0.04-0.01+GReqfactor+0.1) * 1.5;}
-    else if( instate =="True" && reqb4=="False"){
-        finalNum = (0.02+GReqfactor+0.1) * 1.5;}
-    else if (instate=="False" && reqb4 =="False"){
-        finalNum =(0.04+GReqfactor+0.1) * 1.5;}
-    //else
-      //  finalNum = 100000;
-    
-   // return finalNum;
+        if (instate== "True" && reqb4 =="False"){
+            finalNum =(0.02-0.01+GReqfactor+0.1) * 1.5;}
+        else if (instate == "False" && reqb4 =="True"){
+            finalNum = (0.04-0.01+GReqfactor+0.1) * 1.5;}
+        else if( instate =="True" && reqb4=="False"){
+            finalNum = (0.02+GReqfactor+0.1) * 1.5;}
+        else if (instate=="False" && reqb4 =="False"){
+            finalNum =(0.04+GReqfactor+0.1) * 1.5;}
+        //else
+        //  finalNum = 100000;
+        
+    // return finalNum;
 
-   getsug=1.5+finalNum;
-   payme=getsug*amount;
-   sum=parseFloat(payme).toFixed(2);//fix decimal to 2 places
+    getsug=1.5+finalNum;
+    payme=getsug*amount;
+    sum=parseFloat(payme).toFixed(2);//fix decimal to 2 places
 
-   //instate+="love";
-   //address2+="hate";
-   price_back.innerHTML=getsug;
-   total_back.innerHTML=sum;
+    //instate+="love";
+    //address2+="hate";
+    price_back.innerHTML=getsug;
+    total_back.innerHTML=sum;
 
 
-   var server_data =[
-       {"amount":amount},
-       {"instate":instate},
-       {"reqb4":reqb4},
-       {"price_back.innerHTML":price_back},
-       {"total_back.innerHTML":total_back}
-   ];
+    var server_data =[
+        {"amount":amount},
+        {"date":date},
+        {"instate":instate},
+        {"reqb4":reqb4},
+        {"price_back.innerHTML":price_back},
+        {"total_back.innerHTML":total_back}
+    ];
 
-   $.ajax({
-       type: "POST",
-       url: "/process_suggest", //maps to route decorator for a particualr function in python that processes the request
-       data: JSON.stringify(server_data), //convert JSON(javascript object notation) into srting format to transfer to the server
-       contentType:"appplication/json",
-       dataType: 'json',
-   });
-
+    $.ajax({
+        type: "POST",
+        url: "/process_suggest", //maps to route decorator for a particualr function in python that processes the request
+        data: JSON.stringify(server_data), //convert JSON(javascript object notation) into srting format to transfer to the server
+        contentType:"appplication/json",
+        dataType: 'json',
+    });
+    }
 }
 
