76c4bd45c4987319bfa747016575195d3a55927c
evan343 <evanwu343@gmail.com>
2022-05-06 14:25:27 -0500

    Updated by Evan

--- test_app.py
+++ test_app.py
@@ -20,24 +20,24 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get("/")
         assert response.status_code == 200
         assert "<title>QuotEZ</title>" in response.get_data(as_text=True)
-        
+   
     def test_signup(self):
         response = self.client.get("/registration")
         assert response.status_code == 200
         assert "<title>Registration</title>" in response.get_data(as_text=True)
-        
+      
     def test_signup2(self):
         username = "whatthis111"
         password = "What1234"
         response = self.client.post("/registration", data={"usrname": username, "psw": password, "psw-con": password})
         assert response.status_code == 302
-    
+
     def test_signup22(self):
         username = "test"
         password = "Abc12345"
         response = self.client.post("/registration", data={"usrname": username, "psw": password, "psw-con": password})
         assert response.status_code == 302
-    
+
     def test_signup222(self):
         username = "test1"
         password = "Abc12345"
@@ -49,7 +49,7 @@ class AppTestCase(unittest.TestCase):
         password = "Nani1234"
         response = self.client.post("/registration", data={"usrname": username, "psw": password, "psw-con": password})
         assert response.status_code == 302
-        
+
     def test_signup4(self):
         username = "ADMIN"
         password = "123456"
@@ -61,14 +61,14 @@ class AppTestCase(unittest.TestCase):
         password = "Signup5123"
         response = self.client.post("/registration", data={"usrname": username, "psw": password, "psw-con": password})
         assert response.status_code == 302
-    
+
     def test_login(self):
         username = "123459"
         password = "212341235413451345"
         response = self.client.get("/login")
         assert response.status_code == 200
         assert "<title>Login</title>" in  response.get_data(as_text=True)
-        
+   
     def test_login2(self):
         username = "test"
         password = "Abc12345"
@@ -110,7 +110,7 @@ class AppTestCase(unittest.TestCase):
         assert response.status_code == 302
         response = self.client.get("/land")
         assert response
-        
+
     def test_profile(self):
         username = "admin"
         password = "Nani1234"
@@ -118,7 +118,7 @@ class AppTestCase(unittest.TestCase):
         assert response.status_code == 302
         response = self.client.get("/profile")
         assert response
-    
+
     def test_profile1(self):
         with self.client.session_transaction() as sess:
             sess['user'] = 'test'
@@ -126,7 +126,6 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get("/profile")
         assert response#.status_code == 200
 
-        
     def test_profile2(self):
         with self.client.session_transaction() as sess:
             sess['user'] = 'test'
@@ -135,7 +134,7 @@ class AppTestCase(unittest.TestCase):
         response = self.client.post("/profile", data=profile)
         assert response.status_code == 200
         assert  "Zipcode is too short" in response.get_data(as_text=True)
-        
+   
     def test_profile3(self):
         with self.client.session_transaction() as sess:
             sess['user'] = 'test'
@@ -143,8 +142,8 @@ class AppTestCase(unittest.TestCase):
         profile = {'name': '1234', 'address1': '1', 'address2': '2', 'city': 'city', 'zipcode': '12341234', 'state': 'al'}
         response = self.client.post("/profile", data=profile)
         assert response.status_code == 302
-        
-        
+
+
 
     def test_quote0(self):
         response = self.client.get("/quote")
@@ -157,26 +156,39 @@ class AppTestCase(unittest.TestCase):
         response = self.client.get("/quote")
         assert response
         #assert "please complete your profile first!" in response.get_data(as_text=True)
-        
+
     def test_quote_2(self):
+        username = "admin"
+        password = "Nani1234"
+        response = self.client.post("/login", data={"username": username, 'psw': password})
+        assert response.status_code == 302
         with self.client.session_transaction() as sess:
-            sess['user'] = 'test'
-        quote = {'number': '1234', 'date': '5000', 'address': '11111', 'price': '50'}
+            sess['user'] = 'admin'
+        quote = {'number': '1234', 'date': '9999-12-12', 'address': '11111', 'price': '50'}
         response = self.client.post("/quote", data=quote)
         assert response
 
-    
+    def test_quote_3(self):
+        username = "admin"
+        password = "Nani1234"
+        response = self.client.post("/login", data={"username": username, 'psw': password})
+        assert response.status_code == 302
+        with self.client.session_transaction() as sess:
+            sess['user'] = 'admin'
+        quote = {'number': '1234', 'date': '9999-12-12', 'address': '11111', 'price': '50'}
+        response = self.client.get("/quote", data=quote)
+        assert response
+
     def test_quote_history(self):
         with self.client.session_transaction() as sess:
             sess['user'] = 'test'
 
         response = self.client.get("/quote_history")
         assert response.status_code == 200
-        
+  
     def test_quote_history1(self):
         response = self.client.get("/quote_history")
         assert response.status_code == 302
-
 
 if __name__ == "__main__":
     unittest.main()
